f053c242803c8924debbdbaa52b567f1460aaeb6
William Volin <me@williamvolin.com>
2023-08-27 16:26:51 -0700

    add subbed combatcontroller

--- engine/combat/combat_and_move.py
+++ engine/combat/combat_and_move.py
@@ -1,8 +1,7 @@
-from control import sos_ctrl
-from engine.combat.utility.sos_reasoner import SoSReasoner
+from engine.combat.combat_controller import CombatController
 from engine.mathlib import Vec3
 from engine.seq.move import InteractMove, SeqMove
-from memory.combat_manager import CombatCharacter, combat_manager_handle
+from memory.combat_manager import combat_manager_handle
 
 combat_manager = combat_manager_handle()
 
@@ -27,74 +26,10 @@ class SeqCombatAndMove(SeqMove):
             # If there is no active fight, move along the designated path
             super().navigate_to_checkpoint(delta)
         else:
-            # Manual control, do nothing
-            self.execute_combat(delta)
+            CombatController().execute_combat(delta)
 
     # Mash through cutscene while holding the turbo button
     def execute_combat(self, delta: float) -> bool:
-        ctrl = sos_ctrl()
-        ctrl.set_neutral()
-        self.timer = self.timer + delta
-        # if combat is done, just exit
-        if combat_manager.encounter_done is True:
-            return False
-
-        # if reasoner doesn't exist, add it.
-        if combat_manager.encounter_done is False and self.reasoner is None:
-            self.reasoner = SoSReasoner(combat_manager)
-
-        # if we dont have an action or the current appraisal is complete,
-        # we make a new one.
-        # we also check if battle command has focus, so it doesn't start executing before
-        # we have control
-        if (
-            (self.action is None or self.action.appraisal.complete)
-            and combat_manager.selected_character is not CombatCharacter.NONE
-            and combat_manager.battle_command_has_focus
-        ):
-            # print("No action exists, executing one one")
-            self.action = self.reasoner.execute()
-            return False
-
-        # For some reason the action isn't set, so bail out.
-        if self.action is None:
-            # print("baling out because self action is nil")
-            return False
-
-        # if the consideration doesn't believe the situation is valid, execute it.
-        # This will put the cursor on the character it should be on.
-        # internally it checks to see if the character is not NONE and if the selected
-        # character is the one it wants and return true if so.
-        # if the character is None, it knows it can't move things.
-        consideration_valid = self.action.consideration.valid(
-            combat_manager.selected_character, self.action
-        )
-        if not consideration_valid:
-            # print("Consideration is not valid, move cursor")
-            self.action.consideration.execute()
-            return False
-
-        # do we need to navigate to an action?
-        # if we are on the selected character, run the appraisal:
-        if consideration_valid:
-            # print("Try to execute the appraisal")
-            self.action.appraisal.execute()
-            if self.action.appraisal.complete:
-                # print("appraisal is complete, reset action")
-                self.action = None
-            return False
-        # are we waiting for an attack to complete?
-
-        # is an enemy attacking - do we need to defend?
-
-        # execute consideration; it should know what states it expected
-        # if consideration not executed, execute it
-        #   - considerations must have actions, it will pop an action off the stack
-        #     and run it so the ui is not blocked. once the stack is complete it will mark
-        #     the consideration as completed and will continue on.
-        # if consideration executed
-
-        # Check if we have control
         return combat_manager.encounter_done is True
 
     def __repr__(self) -> str:
---
+++ engine/combat/combat_controller.py
@@ -0,0 +1,74 @@
+from control import sos_ctrl
+from engine.combat.utility.sos_reasoner import SoSReasoner
+from memory.combat_manager import CombatCharacter, combat_manager_handle
+
+combat_manager = combat_manager_handle()
+
+
+class CombatController:
+    # returns a bool to feed to the sequencer
+    def execute_combat(self) -> bool:
+        ctrl = sos_ctrl()
+        ctrl.set_neutral()
+
+        # if combat is done, just exit
+        if combat_manager.encounter_done is True:
+            return False
+
+        # if reasoner doesn't exist, add it.
+        if combat_manager.encounter_done is False and self.reasoner is None:
+            self.reasoner = SoSReasoner(combat_manager)
+
+        # if we dont have an action or the current appraisal is complete,
+        # we make a new one.
+        # we also check if battle command has focus, so it doesn't start executing before
+        # we have control
+        if (
+            (self.action is None or self.action.appraisal.complete)
+            and combat_manager.selected_character is not CombatCharacter.NONE
+            and combat_manager.battle_command_has_focus
+        ):
+            # print("No action exists, executing one one")
+            self.action = self.reasoner.execute()
+            return False
+
+        # For some reason the action isn't set, so bail out.
+        if self.action is None:
+            # print("baling out because self action is nil")
+            return False
+
+        # if the consideration doesn't believe the situation is valid, execute it.
+        # This will put the cursor on the character it should be on.
+        # internally it checks to see if the character is not NONE and if the selected
+        # character is the one it wants and return true if so.
+        # if the character is None, it knows it can't move things.
+        consideration_valid = self.action.consideration.valid(
+            combat_manager.selected_character, self.action
+        )
+        if not consideration_valid:
+            # print("Consideration is not valid, move cursor")
+            self.action.consideration.execute()
+            return False
+
+        # do we need to navigate to an action?
+        # if we are on the selected character, run the appraisal:
+        if consideration_valid:
+            # print("Try to execute the appraisal")
+            self.action.appraisal.execute()
+            if self.action.appraisal.complete:
+                # print("appraisal is complete, reset action")
+                self.action = None
+            return False
+        return None
+        # are we waiting for an attack to complete?
+
+        # is an enemy attacking - do we need to defend?
+
+        # execute consideration; it should know what states it expected
+        # if consideration not executed, execute it
+        #   - considerations must have actions, it will pop an action off the stack
+        #     and run it so the ui is not blocked. once the stack is complete it will mark
+        #     the consideration as completed and will continue on.
+        # if consideration executed
+
+        # Check if we have control
