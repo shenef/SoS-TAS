b04bf040945c20e977ffcc3254543c0bfcb2e8e3
William Volin <me@williamvolin.com>
2023-09-08 21:08:41 -0700

    remove self.step from basicattack

--- engine/combat/appraisals/basic_attack.py
+++ engine/combat/appraisals/basic_attack.py
@@ -2,7 +2,6 @@ import logging
 
 from engine.combat.utility.sos_appraisal import (
     SoSAppraisal,
-    SoSAppraisalStep,
     SoSBattleCommand,
     SoSTargetType,
     SoSTimingType,
@@ -15,7 +14,6 @@ class BasicAttack(SoSAppraisal):
     def __init__(self):
         super().__init__()
         self.timing_type = SoSTimingType.OneHit
-        self.step = SoSAppraisalStep.SelectingCommand
         self.battle_command = SoSBattleCommand.Attack
         self.target_type = SoSTargetType.Enemy
 
