9e0e144ae92c03c7d66e716b3a1c1943a1c17d97
William Volin <me@williamvolin.com>
2023-09-18 00:08:41 -0700

    Add initial block system
    - only blocks slugs and spam blocks any cast
    
    removes noise

--- memory/combat_manager.py
+++ memory/combat_manager.py
@@ -1,10 +1,13 @@
 import contextlib
+import logging
 from enum import Enum, auto
 from typing import Self
 
 from memory.core import mem_handle
 from memory.mappers.enemy_name import EnemyName
 from memory.mappers.player_party_character import PlayerPartyCharacter
+
+logger = logging.getLogger(__name__)
 
 
 class CombatDamageType(Enum):
@@ -55,6 +58,21 @@ class CombatEnemyTarget:
         self.spell_locks = []
 
 
+class NextCombatEnemy:
+    def __init__(self: Self) -> None:
+        self.unique_id = None
+        self.enemy = None
+        self.state_type = NextCombatAction.NONE
+        self.move_name = ""
+        self.movement_done = False
+
+
+class NextCombatAction(Enum):
+    NONE = auto()
+    Casting = auto()
+    Attacking = auto()
+
+
 class CombatPlayer:
     def __init__(self: Self) -> None:
         self.max_hp = None
@@ -97,6 +115,7 @@ class CombatManager:
         self.skill_command_index = None
         self.selected_attack_target_guid = None
         self.selected_skill_target_guid = None
+        self.next_combat_enemy = None
         # Moonerang
         self.projectile_hit_count = 0
         self.projectile_speed = 0.0
@@ -138,6 +157,85 @@ class CombatManager:
         except Exception as e:  # noqa: F841
             # logger.debug(f"Combat Manager Reloading - {type(e)}")
             self.__init__()
+
+    def read_next_combat_enemy(self: Self) -> None:
+        if self._should_update():
+            try:
+                ongoing_move_ptr = self.memory.follow_pointer(
+                    self.base,
+                    [
+                        self.current_encounter_base,
+                        0x120,
+                        0x58,
+                        0x10,
+                        0x20,
+                        0x68,
+                        0x0,
+                    ],
+                )
+                combat_move_ptr = self.memory.follow_pointer(
+                    self.base,
+                    [
+                        self.current_encounter_base,
+                        0x120,
+                        0x58,
+                        0x10,
+                        0x20,
+                        0x58,
+                        0x0,
+                    ],
+                )
+                is_player = self.memory.read_bool(ongoing_move_ptr + 0xB8)
+                if is_player:
+                    self.next_combat_enemy = None
+                    return
+                spell_power = self.memory.read_float(combat_move_ptr + 0x30)
+                guid_ptr = self.memory.follow_pointer(
+                    ongoing_move_ptr, [0xF8, 0xF0, 0x18, 0x0]
+                )
+                guid_ptr = self.memory.follow_pointer(
+                    ongoing_move_ptr, [0xF8, 0xF0, 0x18, 0x0]
+                )
+                guid = self.memory.read_uuid(guid_ptr + 0x14)
+
+                current_state_ptr = self.memory.follow_pointer(
+                    ongoing_move_ptr,
+                    [0x80, 0x50, 0x0],
+                )
+
+                state_name_ptr = self.memory.follow_pointer(
+                    current_state_ptr, [0x10, 0x48, 0x0]
+                )
+                state_name = self.memory.read_string(state_name_ptr, 28)
+                move_ptr = self.memory.follow_pointer(
+                    combat_move_ptr, [0x90, 0x18, 0x10, 0x20, 0x18, 0x0]
+                )
+                move_length = self.memory.read_int(move_ptr + 0x10)
+                move_name = self.memory.read_string(move_ptr + 0x14, move_length * 2)
+                next_combat_enemy = NextCombatEnemy()
+                next_combat_enemy.movement_done = False
+                next_combat_enemy.move_name = move_name.replace("\x00", "")
+                next_combat_enemy.state_name = state_name
+
+                for enemy in self.enemies:
+                    if enemy.unique_id == guid:
+                        next_combat_enemy.enemy = enemy
+
+                # if turns > 0 and name does not contain casting
+
+                if next_combat_enemy.enemy and spell_power > 0.0:
+                    next_combat_enemy.state_type = NextCombatAction.Casting
+                else:
+                    next_combat_enemy.state_type = NextCombatAction.Attacking
+                    next_combat_enemy.movement_done = self.memory.read_bool(
+                        current_state_ptr + 0x11A
+                    )
+
+                self.next_combat_enemy = next_combat_enemy
+                return
+            except Exception:
+                self.next_combat_enemy = None
+        self.next_combat_enemy = None
 
     # Helper function for updating itself and ensuring an internal function doesn't run without
     # the base. This is different than other modules as an attempt to improve performance of the
