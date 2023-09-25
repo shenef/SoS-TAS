eab81580dc0552f2f4040554bda88f165ccb0b69
orkaboy <orkaboy@gmail.com>
2023-09-25 20:27:17 +0200

    Minor optimization to minimize time moving on ledge

--- route/evermist_island/mountain_trail.py
+++ route/evermist_island/mountain_trail.py
@@ -77,13 +77,13 @@ class IntroMountainTrail(SeqList):
                     coords=[
                         Vec3(-82.388, 10.002, 37.691),
                         Vec3(-82.254, 10.002, 42.898),
-                        Vec3(-87.191, 10.002, 44.145),
+                        Vec3(-88.219, 10.002, 44.546),
                     ],
                 ),
                 SeqClimb(
                     name="Climb cliff",
                     coords=[
-                        InteractMove(-87.191, 16.000, 44.900),
+                        InteractMove(-88.219, 16.000, 45.000),
                     ],
                 ),
                 SeqCliffMove(
