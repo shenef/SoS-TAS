145c9cdc52458c00519aaf5ba3f7eb5e290f02d3
orkaboy <orkaboy@gmail.com>
2023-09-29 08:35:51 +0200

    Docstrings for sequencer and base nodes

--- engine/seq/sequencer.py
+++ engine/seq/sequencer.py
@@ -1,3 +1,21 @@
+"""
+TAS execution engine.
+
+This code is the core of the TAS run, and executes a tree of
+sequencer nodes (`SeqBase` and classes that inherit from it).
+
+Each node is some kind of action for the TAS to take, and can
+have varying complexity:
+
+Some examples:
+
+    `SeqInteract`, presses the confirm button once.
+    `SeqMove`, navigates a list of game coordinates.
+    `SeqList`, iterate over a list of other sequencer nodes.
+
+`SeqList` in particular can be used to create a hierarchical tree of nodes.
+"""
+
 # Libraries and Core Files
 import datetime
 import logging
@@ -18,7 +36,7 @@ class SequencerEngine:
     """
     Engine for executing sequences of generic TAS events.
 
-    Each event sequence can be nested using SeqList.
+    Each event sequence can be nested using `SeqList`.
     """
 
     def __init__(self: Self, window: Window, config: dict, root: SeqBase) -> None:
@@ -29,15 +47,23 @@ class SequencerEngine:
         self.paused = False
         self.timestamp = time.time()
 
+    # TODO(orkaboy): Not really used at this point (we just create a new sequencer instead).
     def reset(self: Self) -> None:
+        """Reset the sequencer."""
         self.paused = False
         self.done = False
         self.root.reset()
 
     def advance_to_checkpoint(self: Self, checkpoint: str) -> bool:
+        """
+        Advance the sequencer to a particular SeqCheckpoint in the tree.
+
+        Can be used to load game into a particular segment of the route.
+        """
         return self.root.advance_to_checkpoint(checkpoint=checkpoint)
 
     def pause(self: Self) -> None:
+        """Pause the execution of the TAS, releasing all gamepad buttons."""
         ctrl = sos_ctrl()
         # Restore controls to neutral state
         ctrl.dpad.none()
@@ -49,6 +75,7 @@ class SequencerEngine:
         logger.info("------------------------")
 
     def unpause(self: Self) -> None:
+        """Resume execution of the TAS when paused."""
         self.paused = False
         self.timestamp = time.time()
         logger.info("------------------------")
@@ -62,7 +89,7 @@ class SequencerEngine:
         return delta
 
     def _update(self: Self) -> None:
-        # Execute current gamestate logic
+        """Execute current gamestate logic."""
         if not self.paused:
             delta = self._get_deltatime()
             self.done = self.root.execute(delta=delta)
@@ -78,6 +105,7 @@ class SequencerEngine:
         imgui.text_wrapped(f"Time: {timestamp}{pause_str}")
 
     def _render(self: Self) -> None:
+        """Render the state of the sequencer in an imgui window."""
         imgui.text_wrapped(f"TAS version: {TAS_VERSION_STRING} |")
         imgui.same_line()
         # Render timer and gamestate tree
@@ -92,8 +120,8 @@ class SequencerEngine:
         # Render the current gamestate
         self.root.render()
 
-    # Execute and render TAS progress
     def run(self: Self) -> bool:
+        """Execute and render TAS progress."""
         self._update()
         self._render()
 
