d88f5c1bb5dc34bc314173a379ce8b269a8680dd
orkaboy <orkaboy@gmail.com>
2023-10-07 12:07:57 +0200

    Fix for jumping into mines. Routing up wall.

--- route/sleeper_island/stonemasons.py
+++ route/sleeper_island/stonemasons.py
@@ -9,6 +9,8 @@ from engine.seq import (
     InteractMove,
     SeqAwaitLostControl,
     SeqCheckpoint,
+    SeqClimb,
+    SeqHoldDirectionUntilClose,
     SeqHoldDirectionUntilLostControl,
     SeqInteract,
     SeqList,
@@ -46,6 +48,8 @@ class MoveToMines(SeqList):
                 ),
                 SeqInteract("Jump into pit"),
                 SeqAwaitLostControl("Wait for cutscene"),
+                SeqSkipUntilIdle("Screen transition"),
+                SeqAwaitLostControl("Wait for cutscene"),
                 SeqSkipUntilIdle("Elder"),
                 SeqMove(
                     name="Leave elevator",
@@ -73,9 +77,42 @@ class WindTunnelMines(SeqList):
                         Vec3(26.299, 14.002, 20.634),
                         HoldDirection(33.500, 1.002, 76.500, joy_dir=Vec2(0, 1)),
                         Vec3(33.500, 1.002, 93.857),
-                        # TODO(orkaboy): Continue routing
+                        Vec3(36.303, 1.002, 99.433),
+                        Vec3(36.072, 1.002, 115.042),
                     ],
                 ),
+                SeqInteract("Lever"),
+                SeqMove(
+                    name="Move to wall",
+                    coords=[
+                        Vec3(36.407, 1.002, 101.482),
+                        Vec3(44.240, 1.002, 101.419),
+                        InteractMove(46.270, 3.002, 103.390),
+                        Vec3(46.549, 3.002, 104.743),
+                    ],
+                ),
+                SeqClimb(
+                    name="Climb wall",
+                    coords=[
+                        InteractMove(46.712, 11.294, 104.623),
+                        Vec3(44.463, 10.010, 106.873),
+                    ],
+                ),
+                SeqMove(
+                    name="Jump onto pillar",
+                    coords=[
+                        InteractMove(43.688, 10.002, 106.959),
+                        Vec3(37.118, 10.002, 112.922),
+                        InteractMove(31.539, 10.002, 112.922),
+                    ],
+                ),
+                SeqHoldDirectionUntilClose(
+                    name="Wait for pillar to depress",
+                    target=Vec3(31.539, 3.020, 112.922),
+                    joy_dir=Vec2(1, 0),
+                    precision=0.2,
+                ),
+                # TODO(orkaboy): Continue routing
             ],
         )
 
