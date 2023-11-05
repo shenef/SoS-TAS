4a6add587b69db8c21938b69c5d9cdc0cf5093f7
orkaboy <orkaboy@gmail.com>
2023-11-05 12:27:05 +0100

    Adjusted boat movement code slightly

--- engine/seq/move.py
+++ engine/seq/move.py
@@ -504,10 +504,16 @@ class SeqBoat(SeqMove):
         self.hold_skip = hold_skip
 
     def player_position(self: Self) -> Vec3:
-        return boat_manager.position
+        boat_pos = boat_manager.position
+        if boat_pos is None:
+            return Vec3(0.0, 0.0, 0.0)
+        return boat_pos
 
     def move_function(self: Self, player_pos: Vec3, target_pos: Vec3) -> None:
         ctrl = sos_ctrl()
+        if self.hold_skip:
+            ctrl.toggle_turbo(state=True)
+            ctrl.toggle_confirm(state=True)
         # Get the trajectory between the player pos and the target
         target_vec = target_pos - player_pos
         target_vec_v2 = Vec2(target_vec.x, target_vec.z)
@@ -525,9 +531,6 @@ class SeqBoat(SeqMove):
 
         ctrl.set_joystick(joy)
         ctrl.toggle_bracelet(state=True)
-        if self.hold_skip:
-            ctrl.toggle_turbo(state=True)
-            ctrl.toggle_confirm(state=True)
 
     def on_done(self: Self) -> None:
         ctrl = sos_ctrl()
