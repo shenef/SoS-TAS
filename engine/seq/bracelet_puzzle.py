7464596c07c018e8e9a22964f3a7e1659b521c74
orkaboy <orkaboy@gmail.com>
2023-11-13 19:53:14 +0100

    Cleanup of old logic

--- engine/seq/bracelet_puzzle.py
+++ engine/seq/bracelet_puzzle.py
@@ -4,12 +4,9 @@ import logging
 from collections.abc import Callable
 from typing import Self
 
-from imgui_bundle import imgui
-
 from control import sos_ctrl
-from engine.mathlib import Vec2, Vec3
-from engine.seq.base import SeqBase
-from engine.seq.move import move_to
+from engine.mathlib import Vec3
+from engine.seq.move import MistralBracelet, SeqMoveBase
 from memory import (
     player_party_manager_handle,
 )
@@ -19,15 +16,7 @@ logger = logging.getLogger(__name__)
 player_party_manager = player_party_manager_handle()
 
 
-class MistralBracelet:
-    """Action: Use Mistral Bracelet in a given direction."""
-
-    def __init__(self: Self, joy_dir: Vec2, timeout_s: float = 0.1) -> None:
-        self.joy_dir = joy_dir
-        self.timeout_s = timeout_s
-
-
-class SeqBraceletPuzzle(SeqBase):
+class SeqBraceletPuzzle(SeqMoveBase):
     """Sequence to perform a bracelet puzzle."""
 
     def __init__(
@@ -37,126 +26,40 @@ class SeqBraceletPuzzle(SeqBase):
         precision: float = 0.2,
         func: Callable = None,
     ) -> None:
-        self.step = 0
-        self.coords = coords
-        # Used for detecting endpoint of Vec3 movement
-        self.precision = precision
         # Interact variables
-        self.timer = 0
-        super().__init__(name, func=func)
-
-    def reset(self: Self) -> None:
-        """Reset node."""
-        self.step = 0
-        self.timer = 0
-
-    def _nav_done(self: Self) -> bool:
-        num_coords = len(self.coords)
-        # If we are already done with the entire sequence, terminate early
-        return self.step >= num_coords
-
-    def _move_function(self: Self, player_pos: Vec3, target_pos: Vec3) -> None:
-        move_to(
-            player=Vec2(player_pos.x, player_pos.z),
-            target=Vec2(target_pos.x, target_pos.z),
-            running=True,
-            invert=False,
-        )
-
-    def _player_position(self: Self) -> Vec3:
-        return player_party_manager.position
+        super().__init__(name, func=func, coords=coords, precision=precision)
 
     def handle_movement(self: Self, player_pos: Vec3, target: Vec3) -> None:
-        """Handle movement between bracelet uses."""
+        """Handle movement between block pushes."""
         # If arrived, go to next coordinate in the list
-        if Vec3.is_close(player_pos, target, self.precision):
-            logger.debug(
-                f"Checkpoint {self.step}, "
-                + f"Δ: {Vec3.dist(player_pos, target):.3f}, "
-                + f"Pos: {player_pos}, "
-                + f"{target}"
-            )
-            self.step = self.step + 1
-            # Reset state variables
-            self.timer = 0
-        else:
-            self._move_function(player_pos=player_pos, target_pos=target)
+
+        if not self.advance_checkpoint(player_pos, target):
+            self.move_function(player_pos=player_pos, target_pos=target)
 
     def navigate_to_checkpoint(self: Self, delta: float) -> None:
-        """Handle bracelet logic."""
+        """Handle pushing blocks."""
         # Move towards target
-        if self.step >= len(self.coords):
+        target = self.get_target()
+        if target is None:
             return
-        target = self.coords[self.step]
 
-        player_pos = self._player_position()
+        player_pos = self.player_position()
         if player_pos.x is None:
             return
 
         if isinstance(target, MistralBracelet):
             ctrl = sos_ctrl()
             ctrl.set_joystick(target.joy_dir)
-            self.timer += delta
-            if self.timer >= target.timeout_s:
-                self.timer = 0
+            if self.update_timer(delta, target.timeout_s):
                 ctrl.set_neutral()
                 ctrl.bracelet()
-                self.step = self.step + 1
+                self.next_step()
         else:
             self.handle_movement(player_pos, target)
 
     def execute(self: Self, delta: float) -> bool:
         """Top level execution function, called by Sequencer."""
-        self.navigate_to_checkpoint(delta)
-
-        done = self._nav_done()
-
+        done = super().execute(delta)
         if done:
-            logger.info(f"Finished bracelet puzzle section: {self.name}")
-            self._on_done()
+            logger.info(f"Finished block puzzle section: {self.name}")
         return done
-
-    def _on_done(self: Self) -> None:
-        sos_ctrl().set_neutral()
-
-    def __repr__(self: Self) -> str:
-        num_coords = len(self.coords)
-        if self.step >= num_coords:
-            return f"Block Puzzle - {self.name} [{num_coords}/{num_coords}]"
-        target = self.coords[self.step]
-        step = self.step + 1
-        return f"Block Puzzle - {self.name} [{step}/{num_coords}]: {target}"
-
-    def render_tree(self: Self, parent_path: str, selected: bool) -> None:
-        """Render imgui tree view."""
-        imgui.push_id(parent_path)
-        if selected:
-            imgui.push_style_color(imgui.Col_.text, imgui.ImVec4(0.1, 0.9, 0.1, 1.0))
-        tree_node = imgui.tree_node_ex(
-            f"{self.__class__.__name__}({self.name})", imgui.TreeNodeFlags_.span_full_width
-        )
-        if selected:
-            imgui.pop_style_color()
-            if imgui.button("Skip"):
-                self.skip = True
-        if tree_node:
-            self.render_coords(selected)
-            imgui.tree_pop()
-        imgui.pop_id()
-
-    def render_coords(self: Self, selected: bool) -> None:
-        """Render imgui tree leaf (coords)."""
-        for idx, coord in enumerate(self.coords):
-            child_selected = selected and idx == self.step
-            if child_selected:
-                imgui.push_style_color(imgui.Col_.text, imgui.ImVec4(0.1, 0.5, 0.1, 1.0))
-            imgui.tree_node_ex(
-                f"[{idx}] {coord}",
-                imgui.TreeNodeFlags_.no_tree_push_on_open
-                | imgui.TreeNodeFlags_.leaf
-                | imgui.TreeNodeFlags_.span_full_width,
-            )
-            if child_selected:
-                imgui.pop_style_color()
-                if imgui.button("Skip Coord"):
-                    self.step += 1
